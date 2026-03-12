f58d458f2cfbd068f634d90f31ef63d6de406f93
parthkale3231 <parthkale3231@gmail.com>
2026-03-12 22:53:35 +0530

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://rbackend-g14z.onrender.com/api';
+const API_URL = 'https://rbackend-g14z.onrender.com';
 
 const api = axios.create({
   baseURL: API_URL,
