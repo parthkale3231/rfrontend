3a08808912d3bcfa0390eb95e403c2171c9f1ed5
IF- Parth kale <parthkale3231@gmail.com>
2026-03-12 22:55:51 +0530

    update backend url to rbackend-1

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://rbackend-g14z.onrender.com/api';
+const API_URL = 'https://rbackend-1.onrender.com';
 
 const api = axios.create({
   baseURL: API_URL,
