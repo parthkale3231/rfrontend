0245858de277c85f6599f9de0ada57cc39dc636a
IF- Parth kale <parthkale3231@gmail.com>
2026-03-12 23:07:50 +0530

    fix: append /api to backend url

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://rbackend-1.onrender.com';
+const API_URL = 'https://rbackend-1.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_URL,
